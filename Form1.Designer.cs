c7df1a178e4a6f7091802d351740270e321032c9
xurz97 <328288351@qq.com>
2022-04-07 13:28:38 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SimpleSniffer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(508, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "结束";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(21, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(400, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "选择网卡";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 258);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(893, 104);
+            this.textBox1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(893, 166);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(21, 66);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(893, 166);
+            this.textBox2.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(762, 26);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.Text = "协议选择";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 458);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Name = "Form1";
+            this.Text = "SimpleSniffer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpPcap;
+using SharpPcap.LibPcap;
+using PacketDotNet;
+using System.Threading;
+
+namespace SimpleSniffer
+{
+    public partial class Form1 : Form
+    {
+        private ICaptureDevice device;
+        private List<RawCapture> PacketQueue = new List<RawCapture>();
+        private Queue<PacketShow> packetStrings;
+        private Queue<PacketShow> packetShowStrings;
+        private int Count = 0;
+        private BindingSource bs;
+        private int deviceNumber = 0;
+        public class PacketShow
+        {
+            public RawCapture p;
+            public int Count { get; private set; }
+            public PosixTimeval Timeval { get { return p.Timeval; } }
+            public int Length { get { return p.Data.Length; }  }
+            public string Protocal { get; set; }
+            public string ipProtocal { get; set; }
+            public string srcIP { get; set; }
+            public string destIP { get; set; }
+            public string srcMAC { get; set; }
+            public string destMAC { get; set; }
+            public PacketShow(int count, RawCapture p)
+            {
+                srcIP = null;
+                destIP = null;
+                srcMAC = null;
+                destMAC = null;
+                this.Count = count;
+                this.p = p;
+                var packet = PacketDotNet.Packet.ParsePacket(p.LinkLayerType, p.Data);
+                var ep = (PacketDotNet.EthernetPacket)packet.Extract<PacketDotNet.EthernetPacket>();
+                if (ep != null)
+                {
+                    Protocal = "Ethernet";
+                    srcMAC = ep.SourceHardwareAddress.ToString();
+                    destMAC = ep.DestinationHardwareAddress.ToString();
+                }
+                var ipPacket = (PacketDotNet.IPPacket)packet.Extract<PacketDotNet.IPPacket>();
+                if (ipPacket != null)
+                {
+                    if (ipPacket.Version == IPVersion.IPv4)
+                    {
+                        ipProtocal = "IPv4";
+                    }
+                    if (ipPacket.Version == IPVersion.IPv6)
+                    {
+                        ipProtocal = "IPv6";
+                    }
+                    ipProtocal = ipPacket.Version.ToString();
+                    srcIP = ipPacket.SourceAddress.ToString();
+                    destIP = ipPacket.DestinationAddress.ToString();
+                    var tcpPacket = (PacketDotNet.TcpPacket)packet.Extract<PacketDotNet.TcpPacket>();
+                    if (tcpPacket != null)
+                    {
+                        Protocal = "TCP";
+                        
+                    }
+                    var udp = (PacketDotNet.UdpPacket)packet.Extract<PacketDotNet.UdpPacket>();
+                    if(udp != null)
+                    {
+                        Protocal = "UDP";
+                    }
+                    var icmpv4 = (PacketDotNet.IcmpV4Packet)packet.Extract<PacketDotNet.IcmpV4Packet>();
+                    if(icmpv4 != null)
+                    {
+                        ipProtocal = "ICMPv4";
+                    }
+                    var icmpv6 = (PacketDotNet.IcmpV6Packet)packet.Extract<PacketDotNet.IcmpV6Packet>();
+                    if (icmpv6 != null)
+                    {
+                        ipProtocal = "ICMPv6";
+                    }
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            deviceNumber = comboBox1.SelectedIndex;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var devices = LibPcapLiveDeviceList.Instance;
+            foreach (var dev in devices)
+            {
+                comboBox1.Items.Add(dev.Description);
+            }
+            comboBox2.Items.Add("All");
+            comboBox2.Items.Add("Ethernet");
+            comboBox2.Items.Add("TCP");
+            comboBox2.Items.Add("UDP");
+            comboBox2.Items.Add("ICMPv4");
+            comboBox2.Items.Add("ICMPv6");
+            dataGridView1.ReadOnly= true;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+        }
+        private void StartCapture()
+        {
+            Count = 0;
+            packetStrings = new Queue<PacketShow>();
+            bs = new BindingSource();
+            dataGridView1.DataSource = bs;
+            device = CaptureDeviceList.Instance[deviceNumber];
+            device.OnPacketArrival +=
+                new PacketArrivalEventHandler(device_OnPacketArrival);
+            int readTimeoutMilliseconds = 500;
+            device.Open(DeviceModes.Promiscuous, readTimeoutMilliseconds);
+            device.StartCapture();
+        }
+        private void EndCapture()
+        {
+            if (device != null)
+            {
+                device.StopCapture();
+                device.Close();
+                
+                device.OnPacketArrival -= device_OnPacketArrival;
+                device = null;
+            }
+        }
+        private void device_OnPacketArrival(object sender, PacketCapture e)
+        {
+            var rawPacket = e.GetPacket();
+            PacketQueue.Add(rawPacket);
+            Count++;
+            var newPacketShow = new PacketShow(Count, rawPacket);
+            packetStrings.Enqueue(newPacketShow);
+            textBox2.Text = "抓包中...... 共抓到了"+Count.ToString()+"个包";
+            var packet = Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Hide();
+            textBox2.Show();
+            textBox2.Text = "抓包中...... 共抓到了0个包";
+            StartCapture();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EndCapture();
+            textBox2.Hide();
+            bs.DataSource = null;
+            dataGridView1.Show();
+            ModeShow(); 
+        }
+        private void ModeShow()
+        {
+            if(packetStrings == null)
+            {
+                return;
+            }
+            packetShowStrings = new Queue<PacketShow>();
+            if (mode == 0)
+            {
+                if (packetStrings.Count == 0)
+                {
+                    dataGridView1.Hide();
+                    textBox2.Show();
+                    textBox2.Text = "没有抓到此种类型的包";
+                    return;
+                }
+                dataGridView1.Show();
+                textBox2.Hide();
+                bs.DataSource = packetStrings;
+            }
+            if (mode == 1) //Ethernet
+            {
+                foreach (var packet in packetStrings)
+                {
+                    if (packet.Protocal == "Ethernet")
+                    {
+                        packetShowStrings.Enqueue(packet);
+                    }
+                }
+                if (packetShowStrings.Count == 0)
+                {
+                    dataGridView1.Hide();
+                    textBox2.Show();
+                    textBox2.Text = "没有抓到此种类型的包";
+                    return;
+                }
+                dataGridView1.Show();
+                textBox2.Hide();
+                bs.DataSource = packetShowStrings;
+            }
+            if (mode == 2) //TCP
+            {
+                foreach (var packet in packetStrings)
+                {
+                    if (packet.Protocal == "TCP")
+                    {
+                        packetShowStrings.Enqueue(packet);
+                    }
+                }
+                if (packetShowStrings.Count == 0)
+                {
+                    dataGridView1.Hide();
+                    textBox2.Show();
+                    textBox2.Text = "没有抓到此种类型的包";
+                    return;
+                }
+                dataGridView1.Show();
+                textBox2.Hide();
+                bs.DataSource = packetShowStrings;
+            }
+            if (mode == 3) //UDP
+            {
+                foreach (var packet in packetStrings)
+                {
+                    if (packet.Protocal == "UDP")
+                    {
+                        packetShowStrings.Enqueue(packet);
+                    }
+                }
+                if (packetShowStrings.Count == 0)
+                {
+                    dataGridView1.Hide();
+                    textBox2.Show();
+                    textBox2.Text = "没有抓到此种类型的包";
+                    return;
+                }
+                dataGridView1.Show();
+                textBox2.Hide();
+                bs.DataSource = packetShowStrings;
+            }
+            if (mode == 4) //ICMPv4
+            {
+                foreach (var packet in packetStrings)
+                {
+                    if (packet.ipProtocal == "ICMPv4")
+                    {
+                        packetShowStrings.Enqueue(packet);
+                    }
+                }
+                if (packetShowStrings.Count == 0)
+                {
+                    dataGridView1.Hide();
+                    textBox2.Show();
+                    textBox2.Text = "没有抓到此种类型的包";
+                    return;
+                }
+                dataGridView1.Show();
+                textBox2.Hide();
+                bs.DataSource = packetShowStrings;
+            }
+            if (mode == 5) //ICMPv6
+            {
+                foreach (var packet in packetStrings)
+                {
+                    if (packet.ipProtocal == "ICMPv6")
+                    {
+                        packetShowStrings.Enqueue(packet);
+                    }
+                }
+                if (packetShowStrings.Count == 0)
+                {
+                    dataGridView1.Hide();
+                    textBox2.Show();
+                    textBox2.Text = "没有抓到此种类型的包";
+                    return;
+                }
+                dataGridView1.Show();
+                textBox2.Hide();
+                bs.DataSource = packetShowStrings;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count == 0)
+                return;
+            if (packetStrings.Count == 0)
+                return;
+            if (mode != 0 && packetShowStrings.Count == 0)
+                return;
+            var packetShow = (PacketShow)dataGridView1.Rows[dataGridView1.SelectedCells[0].RowIndex].DataBoundItem;
+            var packet = Packet.ParsePacket(packetShow.p.LinkLayerType, packetShow.p.Data);
+            textBox1.Text = packet.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            EndCapture();
+        }
+        private int mode;
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mode = comboBox2.SelectedIndex;
+            ModeShow();
+        }
+    }
+}
